5d6a41dc30e8eb5424b93f5bb563b766259de204
unknown <123@qq.com>
2020-10-24 13:57:50 +0800

    first commit

---
+++ src/test/java/com/shakespeare/AppTest.java
@@ -0,0 +1,16 @@
+package com.shakespeare;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
